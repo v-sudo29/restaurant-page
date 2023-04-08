81bcb057c48c6e9b4dd943be337fa5931c78188d
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-04-08 13:17:52 -0700

    Change title

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPLugin({
-      title: 'Output Management',
+      title: 'Restaurant Page',
     }),
   ],
   output: {
